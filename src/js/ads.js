2bd47b39c5aa634969c44a6e2059cb49c67cacd4
nb399 <874085857@qq.com>
2020-11-10 18:22:06 +0800

    ads listen

--- src/js/ads.js
+++ src/js/ads.js
@@ -243,6 +243,14 @@ class Ads {
             }
             break;
         }
+        case google.ima.AdEvent.Type.COMPLETE: {
+            console.log('ad COMPLETE:',ad)
+            break;
+        }
+        case google.ima.AdEvent.Type.SKIPPED: {
+            console.log('ad SKIPPED:',ad)
+            break;
+        }
         }
     }
 
